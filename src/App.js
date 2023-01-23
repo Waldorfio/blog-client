0ef8161c21fce1545069e3c29a86ebdc05a37806
Wally <walid.hanifi707@gmail.com>
2023-01-23 15:56:48 +1000

    Add boilerplate html content for landing page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 function App() {
   return (
     <div className="App">
-      hello this is the front-end client
+      <header>
+        <h1>Wally's Blog</h1>
+      </header>
+
+      <div id="content">
+
+        <div class="post">
+          <div class="date">January 23, 2023</div>
+          <h2>My first blog post</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, magna id faucibus faucibus, tellus est malesuada ipsum, non congue justo quam euismod purus. Sed luctus, augue id pellentesque egestas, libero nibh euismod nibh, non pharetra ligula neque id mi. </p>
+        </div>
+
+        <div class="post">
+          <div class="date">January 22, 2023</div>
+          <h2>My second blog post</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, magna id faucibus faucibus, tellus est malesuada ipsum, non congue justo quam euismod purus. Sed luctus, augue id pellentesque egestas, libero nibh euismod nibh, non pharetra ligula neque id mi. </p>
+        </div>
+
+      </div>
+
+      <footer id="footer">
+          <span id='footer-title'>Copyright © 2022 Waldorfio&nbsp;&nbsp;&nbsp;</span>
+      </footer>
     </div>
   );
 }
