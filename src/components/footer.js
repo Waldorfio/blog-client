4ff7651ac2ae69bf9df238e3c6ffdbdc6a6599f7
Wally <walid.hanifi707@gmail.com>
2023-01-24 10:34:49 +1000

    Initial commit

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+        <footer id="footer">
+            <span id='footer-title'>Copyright © 2022 Waldorfio&nbsp;&nbsp;&nbsp;
+            </span>
+        </footer>
+  );
+}
+
+export default Footer;
