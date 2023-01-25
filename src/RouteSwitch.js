b43f9c73f7d250fb9be6107fdd3cf5498d7ebbcc
Walid VM <walid.hanifi707@gmail.com>
2023-01-25 13:58:11 +1000

    Add registration form route

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Post from './Post';
+import Register from './Register';
 
 function RouteSwitch() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/post" element={<Post />} />
       </Routes>
     </BrowserRouter>
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
     <header>
       <Link to="/"><h1>Wally's Blog</h1></Link>
       <button>Log in</button>
-      <button>Register</button>
+      <Link to="register"><button>Register</button></Link>
     </header>
   );
 }
