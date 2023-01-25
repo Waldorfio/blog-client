3882ffd0fb8706b76e928bf2d6717da97ae01a4c
Walid VM <walid.hanifi707@gmail.com>
2023-01-25 15:01:29 +1000

    Create popup for login screen

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header(props) {
   return (
     <header>
       <Link to="/"><h1>Wally's Blog</h1></Link>
-      <button onClick={handleLogin}>Log in</button>
+      <button id='login-btn' onClick={handleLogin}>Log in</button>
       <Link to="register"><button>Register</button></Link>
     </header>
   );
