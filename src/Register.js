cdf130cd197570760fa5f94f963e382621c0ae86
Walid VM <walid.hanifi707@gmail.com>
2023-01-27 17:28:59 +1000

    Pass outlet context props through

--- src/Register.js
+++ src/Register.js
@@ -1,32 +1,62 @@
-import React, { useState } from 'react';
-import './styles/style.css';
-import './styles/style2.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import React from 'react';
+import { useOutletContext } from 'react-router-dom';
 
 function Register() {
+  const [formData, setFormData, users, setUsers] = useOutletContext();
+
+  const addUser = async (e) => {
+    e.preventDefault();
+    try {
+      // Send post request
+      const res = await fetch('https://blog-api-production-6aeb.up.railway.app/users/create', {
+        method: 'POST',
+        headers: { 'Content-type': 'application/json; charset=UTF-8' },
+        body: JSON.stringify(formData),
+      });
+      // Check data
+      const data = await res.json();
+      if (data.success) {
+        console.log('registration successful');
+        setUsers([...users, data.user]); // Append the new user to the user state
+      } else {
+        console.log('data was not a success');
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div className="Register">
 
-      <Header />
-
-      <form action="" method="post" id="register">
+      <form onSubmit={addUser}>
         <label htmlFor="username">Username:</label>
-        <input type="text" id="username" name="username" />
-
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={formData.username}
+          onChange={(e) => setFormData({ ...formData, username: e.target.value })}
+        />
         <label htmlFor="email">Email:</label>
-        <input type="email" id="emai" name="emai" />
-
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+        />
         <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" />
-
-        <label htmlFor="password_confirm">Confirm Password:</label>
-        <input type="password" id="password_confirm" name="password_confirm" />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+        />
 
         <input type="submit" value="Register" />
       </form>
-
-      <Footer />
 
     </div>
   );
