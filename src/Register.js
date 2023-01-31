d573272193141f39a207dd779e31ffff84508ff1
Wally <walid.hanifi707@gmail.com>
2023-01-31 14:01:34 +1000

    Update registration form html

--- src/Register.js
+++ src/Register.js
@@ -33,38 +33,46 @@ function Register() {
   };
 
   return (
-    <div className="Register">
-
-      <form onSubmit={addUser}>
-        <label htmlFor="username">Username:</label>
-        <input
-          type="text"
-          id="username"
-          name="username"
-          value={formData.username}
-          onChange={(e) => setFormData({ ...formData, username: e.target.value })}
-        />
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={formData.email}
-          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-        />
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={formData.password}
-          onChange={(e) => setFormData({ ...formData, password: e.target.value })}
-        />
-
-        <input type="submit" value="Register" />
-      </form>
-
-    </div>
+    <main className="register-main">
+        <div className='user-form' id="register">
+            <h3>Signup</h3>
+            <form className='user-form' onSubmit={addUser}>
+                <input 
+                  type="email" 
+                  id="email"
+                  name="email"
+                  value=""
+                  placeholder="Enter your email" 
+                  value={formData.email}
+                  onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                />
+                <input
+                  type="text"
+                  id="username"
+                  name="username"
+                  value=""
+                  placeholder="Create a username"
+                  value={formData.username}
+                  onChange={(e) => setFormData({ ...formData, username: e.target.value })}
+                />
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  value=""
+                  placeholder="Create a password"
+                  value={formData.password}
+                  onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+                />
+                <input type="submit" value="Signup">
+            </form>
+            <div className="signup">
+                <span className="signup">Already have an account?
+                <label htmlFor="check">Login</label>
+                </span>
+            </div>
+        </div>
+    </main>
   );
 }
 
